b9af965246fcc19e334b8083ab95c0fb62d4f0c0
Huy Vo <huyvohcmc@gmail.com>
2022-04-08 20:51:09 +0700

    Move counter and trigger to local scope of each test case

--- debounce_test.go
+++ debounce_test.go
@@ -10,99 +10,100 @@ import (
 
 
 func Example() {
-	debouce := godebounce.New(10 * time.Second).WithTrigger(func() {
+	debounce := godebounce.New(10 * time.Second).WithTrigger(func() {
 		fmt.Println("Trigger") // Trigger func will be called after 10 seconds from last action.
 	})
 	
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 1") // After 10 seconds, the trigger will be called.
 	})
 	
 	time.Sleep(3 * time.Second)
 	
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 2")
 		// The schedule trigger func of Action 1 will be cleared.
 		// After 10 seconds of action 2, trigger will be called.
 	})
 }
 
-
-var counter uint64
-var trigger = func () {
-	fmt.Println("Trigger")
-	counter++
-}
-var resetCounter = func () {
-	counter = 0
-}
-
 func TestDebounceDoBeforeExpired(t *testing.T) {
-	resetCounter()
-	debouce := godebounce.New(3 * time.Millisecond).WithTrigger(trigger)
+	counter := 0
+	trigger := func() {
+		fmt.Println("Trigger")
+		counter++
+	}
+	debounce := godebounce.New(3 * time.Millisecond).WithTrigger(trigger)
 
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 1")
 	})
 	
 	time.Sleep(2* time.Millisecond)
 	
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 2")
 	})
 
 	time.Sleep(4 * time.Millisecond)
 
 	if counter != 1 {
-			t.Error("Expected count 1, was ", counter)
+		t.Error("Expected count 1, was ", counter)
 	}
 }
 
 func TestDebounceDoAfterExpired(t *testing.T) {
-	resetCounter()
-	debouce := godebounce.New(3 * time.Millisecond).WithTrigger(trigger)
+	counter := 0
+	trigger := func() {
+		fmt.Println("Trigger")
+		counter++
+	}
+	debounce := godebounce.New(3 * time.Millisecond).WithTrigger(trigger)
 
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 1")
 	})
 	
 	time.Sleep(4 * time.Millisecond)
 	
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 2")
 	})
 
 	time.Sleep(4 * time.Millisecond)
 
 	if counter != 2 {
-			t.Error("Expected count 2, was ", counter)
+		t.Error("Expected count 2, was ", counter)
 	}
 }
 
 
 func TestDeounceMixed(t *testing.T) {
-	resetCounter()
+	counter := 0
+	trigger := func() {
+		fmt.Println("Trigger")
+		counter++
+	}
+	debounce := godebounce.New(3 * time.Millisecond).WithTrigger(trigger)
 
-	debouce := godebounce.New(3 * time.Millisecond).WithTrigger(trigger)
-
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 1")
 	})
 	
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 2")
 	})
 
 	time.Sleep(4 * time.Millisecond)
 
-	debouce.Do(func() {
+	debounce.Do(func() {
 		fmt.Println("Action 3")
 	})
 
 	time.Sleep(4 * time.Millisecond)
 
 	if counter != 2 {
-			t.Error("Expected count 2, was ", counter)
+		t.Error("Expected count 2, was ", counter)
 	}
 }
 
