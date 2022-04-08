295f203c205663715ff3735a8935b763b99ae57d
Ro VN <ngocro208@gmail.com>
2022-04-08 16:02:51 +0700

    feat: Init debounce functionalities

--- debounce.go
+++ debounce.go
@@ -1,15 +1,30 @@
 package godebounce
 
+import (
+	"time"
+)
+
 type Debounce struct {
+	timeDuration time.Duration
+	timer *time.Timer
+	trigger func()
 }
 
-func New() *Debounce {
-	return Debounce{}
+func New(duration time.Duration) *Debounce {
+	return &Debounce{timeDuration: duration}
 }
 
-func (d *Debouce) WithTrigger(triggerFunc func()) *Debouce {
+func (d *Debounce) WithTrigger(triggerFunc func()) *Debounce {
+	d.trigger = triggerFunc
 	return d
 }
 
-func (d *Debouce) Do(action func()) {
+func (d *Debounce) Do(action func()) {
+	action()
+
+	if d.timer != nil {
+		d.timer.Stop()
+	}
+	
+	d.timer = time.AfterFunc(d.timeDuration, d.trigger)
 }
