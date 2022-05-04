67214a5d880228c3f7a253a846f6c55b95252788
Huy Vo <huyvohcmc@gmail.com>
2022-05-05 01:59:18 +0700

    Update example

--- src/pages/example.go
+++ src/pages/example.go
@@ -3,23 +3,24 @@ package main
 import (
 	"fmt"
 	"time"
+
 	"github.com/vnteamopen/godebouncer"
 )
 
 func main() {
-	wait := 10 * time.Second
+	wait := 5 * time.Second
 	debouncer := godebouncer.New(wait).WithTriggered(func() {
-		fmt.Println("Trigger") // Triggered func will be called after 10 seconds from last SendSignal().
+		fmt.Println("Trigger") // Triggered func will be called after 5 seconds from last SendSignal().
 	})
 
 	fmt.Println("Action 1")
 	debouncer.SendSignal()
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	fmt.Println("Action 2")
 	debouncer.SendSignal()
-	// After 10 seconds, the trigger will be called.
+	// After 5 seconds, the trigger will be called.
 	// Previous `SendSignal()` will be ignored to trigger the triggered function.
 
 	time.Sleep(10 * time.Second)
